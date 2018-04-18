5875771e6d6fbe2372941cd042615b8c5fb6e3bc
AIT LACHGUER YOUSSEF <youssef.aitlachguer@gmail.com>
2018-04-18 10:14:47 +0200

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  timer : number = 0;
+  intervalId : number;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
   }
 
+  start() {
+    this.intervalId = setInterval(() => {
+      this.timeIt()
+    }, 1000);
+  }
+
+  stop() {
+       clearInterval(this.intervalId);
+       this.intervalId = -1;
+    }
+
+/*  reset() {
+     this.timeReset();
+     this.clearInterval(timer);
+  } */
+
+  startBtn() {
+    if(this.intervalId == -1) {
+          this.start();
+        }
+}
+
+  stopBtn() {
+    this.stop();
+  }
+
+  timeIt() {
+   this.timer++;
+  }
+
+  /*timeReset() {
+    timer = 0;
+    clearInterval(timer);
+  } */
+
+  ngOnInit() {
+    this.start();
+  }
 }
